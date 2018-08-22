eab5672692f3b390127c6ef95a1654bf5ea782a6
Artsiom Gluhovskiy <gluhovskiy.art@gmail.com>
2018-08-22 13:29:58 +0300

    java-se-labs: LeetCode (Longest Substring quiz).

---
+++ src/main/java/org/art/java_core/algorithms/leetcode/LongestSubstring.java
@@ -0,0 +1,75 @@
+package org.art.java_core.algorithms.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+/**
+ * "Longest Substring Without Repeating Characters" quiz solution from LeetCode.
+ */
+public class LongestSubstring {
+
+    public int lengthOfLongestSubstring(String s) {
+        //Two pointers technique
+        int i = 0;
+        int j = 0;
+        int maxLength = 0;
+        Set<Character> chars = new HashSet<>();
+        while (j < s.length()) {
+            if (!chars.contains(s.charAt(j))) {
+                chars.add(s.charAt(j));
+                j++;
+                maxLength = Math.max(maxLength, j - i);
+            } else {
+                chars.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return maxLength;
+    }
+
+    @Test
+    void test1() {
+        String input = "abcabcbb";
+        LongestSubstring solution = new LongestSubstring();
+        assertSame(3, solution.lengthOfLongestSubstring(input));
+    }
+
+    @Test
+    void test2() {
+        String input = "bbbbb";
+        LongestSubstring solution = new LongestSubstring();
+        assertSame(1, solution.lengthOfLongestSubstring(input));
+    }
+
+    @Test
+    void test3() {
+        String input = "pwwkew";
+        LongestSubstring solution = new LongestSubstring();
+        assertSame(3, solution.lengthOfLongestSubstring(input));
+    }
+
+    @Test
+    void test4() {
+        String input = " ";
+        LongestSubstring solution = new LongestSubstring();
+        assertSame(1, solution.lengthOfLongestSubstring(input));
+    }
+
+    @Test
+    void test5() {
+        String input = "dvdf";
+        LongestSubstring solution = new LongestSubstring();
+        assertSame(3, solution.lengthOfLongestSubstring(input));
+    }
+
+    @Test
+    void test6() {
+        String input = "pwwk";
+        LongestSubstring solution = new LongestSubstring();
+        assertSame(2, solution.lengthOfLongestSubstring(input));
+    }
+}

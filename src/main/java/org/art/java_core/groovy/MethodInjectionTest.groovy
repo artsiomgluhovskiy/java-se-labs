2549c837ec0fda9b5160873b306159c69a978fbb
artgluhovskiy <gluhovskiy.art@gmail.com>
2018-11-29 23:33:41 +0300

    Java Labs: Simple Groovy example - method injection.

---
+++ src/main/java/org/art/java_core/groovy/MethodInjectionTest.groovy
@@ -0,0 +1,27 @@
+package org.art.java_core.groovy
+
+class MethodInjectionTest {
+
+    static void main(String[] args) {
+        def person1 = new Person(name: "John Smith", age: 22, salary: 1234d)
+        println person1
+        injectValidateMethod()
+        person1.validate()
+
+        def person2 = new Person(age: 22, salary: 1234d)
+        println person2
+        person2.validate()
+    }
+
+    static void injectValidateMethod() {
+        Person.metaClass.validate = {->
+            delegate.properties.each {property, value ->
+                if (value == null) {
+                    println "Entity validation failed: Property $property is null!"
+                    throw new IllegalArgumentException()
+                }
+            }
+            println "Entity validation succeed!"
+        }
+    }
+}
---
+++ src/main/java/org/art/java_core/groovy/Person.java
@@ -0,0 +1,43 @@
+package org.art.java_core.groovy;
+
+import java.math.BigDecimal;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private BigDecimal salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
